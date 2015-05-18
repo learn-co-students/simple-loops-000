5cc722f43864b0394883ad060112fa90b79c1bc5
Rsenger <Rgermosen12@gmail.com>
2015-05-17 20:40:31 -0400

    Fruit_a_loops

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,22 +1,45 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  # code your solution here using the "loop" keyword
+  	number_of_times = 0
+  	loop do 
+  		number_of_times += 1
+  		puts "Welcome to Flatiron School's Web Development Course!"
+  		break if number_of_times == 7
+  	end
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
+
+ 7.times do 
+ 	puts "Welcome to Flatiron School's Web Development Course!"
+ end
+
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
+ 
+ 	number_of_times = 0
+	while number_of_times < 7
+		puts "Welcome to Flatiron School's Web Development Course!"
+		number_of_times += 1
+	end
+
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
+
+	number_of_times = 7
+	until number_of_times == 0
+		puts "Welcome to Flatiron School's Web Development Course!"
+		number_of_times -= 1
+	end
+
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
+  for number_of_times in 1..7 do
+		puts "Welcome to Flatiron School's Web Development Course!"
+	end
 end
 
